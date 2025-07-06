d35183ba6006e998a9390adabeef000492190f59
Volodymyr Chuklin <xfex2014@gmil.com>
2025-07-06 15:29:57 +0300

    3

--- logger.py
+++ logger.py
@@ -1,11 +1,11 @@
-#logging
+#logger.py
 import logging
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[
-        logging.FileHandler("ppm.log"),
+        logging.FileHandler("ppm.log", encoding='utf-8'),
         logging.StreamHandler()
     ]
 )
